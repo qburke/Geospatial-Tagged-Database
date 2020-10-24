b6a9819e2e3a1de4e55bf0f854c5262d9cabc7d2
Woosang Kang <wk228@cornell.edu>
2020-10-24 11:03:55 +0900

    initial commit

--- rtree.mli
+++ rtree.mli
@@ -10,12 +10,8 @@ val empty : 'a -> 'a t
 (** [new_tree p x] is a new R Tree with one element [x] at [p]. *)
 val new_tree : Point.t -> 'a -> 'a t
 
-<<<<<<< HEAD
 (** [add p x r] modifies [r] to include [x] at location [p]. Requires [x] not in
     [r]. *)
-=======
-(** [add x r] modifies [r] to include [x]. Requires [x] not in [r]. *)
->>>>>>> 5820225... initial commit
 val add : Point.t -> 'a -> 'a t -> unit
 
 (** [remove x r] removes [x] from [r]. Leaves [r] unchanged if [x]
