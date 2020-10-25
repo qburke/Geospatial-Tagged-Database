ff6f3c19c8e5e572556a6ea5ec52994568fa5103
David Needles <dpn28@cornell.edu>
2020-10-24 23:47:27 -0400

    Added Authors and Install file

---
+++ INSTALL.md
@@ -0,0 +1,2 @@
+# How to install
+Run `make build` from the command line.
---
+++ authors.mli
@@ -0,0 +1,39 @@
+(************************************************************ 
+   Copyright (C) 2020 Cornell University.
+   Created by Michael Clarkson (mrc26@cornell.edu) and CS 3110 course staff.
+   You may not redistribute this assignment, distribute any derivatives,
+   or use it for commercial purposes.
+ ************************************************************) 
+
+(** CS 3110 Fall 2020 Final Project
+    @authors Quin Burke (qjb5)
+                       Woosang Kang (wk228)
+                       David Needles (dpn28)
+                       Valene Tjong (vyt3)
+ *)
+
+(************************************************************ 
+
+   Academic Integrity Statement
+
+   I, the person named in the @author comment above, have fully reviewed the
+   course academic integrity policies.  I have adhered to those policies in
+   solving the assignment.
+
+   The policies do permit some limited collaboration among students currently
+   enrolled in the course. If I did engage in such collaboration, here is the
+   list of other students with whom I collaborated, and a brief summary of that
+   collaboration:
+
+   - none
+
+   Here are any deviations from the policies that I want to document, with the
+   understanding that being honest about my mistakes is more honorable than
+   lying about my intellectual work:
+
+   - none
+
+ ************************************************************)  
+
+(** [hours_worked] is the number of hours you worked on this assignment. *)
+val hours_worked : int
