ce5b271679491372e68bf2bbd2f42ea9744c2af5
yueshen99 <vyt3@cornell.edu>
2020-12-15 20:57:14 -0500

    minor updates to circle modules

--- lib/circle.mli
+++ lib/circle.mli
@@ -1,8 +1,6 @@
-open Point
-open Rect
-
 (** [contains c r p] is true if p is contained in circle [c] of radius [r]. *)
 val contains : Point.t -> float -> Point.t -> bool
 
-(** [intersect c r rect] is true if circle with center [c], radius [r] intersects with rect [r]. *)
+(** [intersect c r rect] is true if circle with center [c], 
+    radius [r] intersects with rect [r]. *)
 val intersect : Point.t -> float -> Rect.t -> bool
