d1ef7c6d16ba303b832db597143c34910c1d07c2
Quin Burke <qjb5@cornell.edu>
2020-12-16 23:47:10 -0500

    Add registry and switch to Opium dev version

--- lib/server.ml
+++ lib/server.ml
@@ -1,13 +1,27 @@
-open Opium.Std
+open Opium
 
-(* initialize, query, add, delete *)
+let reg = Registry.init ()
+
+(* query, add, delete *)
+
+let initialize_handler req = 
+  Printf.sprintf "%s" (Registry.add_new reg (Router.param req "name"))
+  |> Response.of_plain_text
+  |> Lwt.return
+
+let add_handler _req = failwith "unimplemented"
+(*let db = Registry.get reg (Router.param req "name")
+  in let entry = Request.to_json req *) (*Lwt.bind 
+                                               (Body.to_string req.body)
+                                               (fun ent -> Db.add db (Entry.from_json (Yojson.Basic.from_string ent)); Lwt.return_unit)*)
 
 let print_param_handler req =
   Printf.sprintf "Hello, %s\n" (Router.param req "name")
-  |> Response.of_string_body
+  |> Response.of_plain_text
   |> Lwt.return
 
 let start () =
   App.empty
   |> App.get "/hello/:name" print_param_handler
+  |> App.get "/initialize/:name" initialize_handler
   |> App.run_command
