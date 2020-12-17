2ebadc2802cbbd08dd6bf46bcf615a002b924db6
Quin Burke <qjb5@cornell.edu>
2020-12-17 16:35:35 -0500

    Improve error handling

--- lib/server.ml
+++ lib/server.ml
@@ -1,117 +1,150 @@
 open Opium
 
+(* FIXME Yojson Basic vs Safe
+   TODO reexamine db function overhead -- search needs db as list? remove needs to find first?
+*)
+
+exception ParamNotFound of string
+exception DbNotLoaded
+exception MalformedTagList
+exception MalformedJSON
+exception EntryNotFound of string
+
+let exn_response exn =
+  let err_msg = match exn with
+    | ParamNotFound p -> "Required parameter \"" ^ p ^ "\" not found"
+    | DbNotLoaded -> "Database not loaded"
+    | Sys_error _ -> "Database not found"
+    | MalformedTagList -> "Tag list must be a JSON array of strings"
+    | MalformedJSON -> "Malformed JSON"
+    | EntryNotFound id -> "Entry with id \"" ^ id ^ "\" not found"
+    | _ -> "Unknown error"
+  in err_msg |> Response.of_plain_text |> Lwt.return
+
 let reg = Registry.init ()
 
+(** [get_req_param k req] gets the value of query param with key [k] in request
+    [req]. Raises [ParamNotFound k] if param is not found in [req]. *)
 let get_req_param key req =
   match Request.query key req with
   | Some k -> k
-  | None -> failwith (key ^ " not found")
+  | None -> raise (ParamNotFound key)
 
+(** [db_from_req req] gets the identifier of the database in [req], by getting
+    the value of query param "db", and returns the database from the registry.
+    Raises DbNotLoaded if not in registry. *)
 let db_from_req req = 
-  get_req_param "db" req
-  |> Registry.get reg
+  try
+    get_req_param "db" req
+    |> Registry.get reg
+  with
+  | Not_found -> raise DbNotLoaded
 
 let initialize_handler req =
-  get_req_param "name" req
-  |> Registry.add_new reg
-  |> Printf.sprintf "%s"
-  |> Response.of_plain_text
-  |> Lwt.return
+  try
+    get_req_param "name" req
+    |> Registry.add_new reg
+    |> Response.of_plain_text
+    |> Lwt.return
+  with
+  | e -> exn_response e
 
 let load_handler req =
-  let name = get_req_param "name" req
-  in let fname = name ^ ".db"
-  in Registry.load_from_file reg fname name
-     |> Printf.sprintf "%s"
-     |> Response.of_plain_text
-     |> Lwt.return
+  try begin 
+    let name = get_req_param "name" req
+    in let fname = name ^ ".db"
+    in
+    Registry.load_from_file reg fname name
+    |> Response.of_plain_text
+    |> Lwt.return
+  end with
+  | e -> exn_response e
 
 let tag_list_from_json json =
   match json with
   | `List lst -> List.map
                    (fun json -> match json with
                       | `String s -> s
-                      | _ -> failwith "Tags must be strings"
+                      | _ -> raise MalformedTagList
                    )
                    lst
-  | _ -> failwith "Tags must be in list"
+  | _ -> raise MalformedTagList
 
 let tag_search_handler req =
-  let db = db_from_req req
-  in let json_req = Request.to_json_exn req
-  in Lwt.bind 
-    json_req
+  Lwt.bind 
+    (Request.to_json req)
     (fun json ->
-       (try begin
-          let tags = tag_list_from_json json
-          in let elems = Db.tag_search db (Db.list_of_elements db) tags
-          in `List (List.map
-                      (fun elem -> elem |> Db.entry_of_element |> Entry.to_json)
-                      elems)
-             |> Yojson.Basic.to_string
-        end with
-        | Failure s -> Printf.printf "%s" s; s
-       )
-       |> Response.of_plain_text
-       |> Lwt.return
+       try begin
+         let json = match json with
+           | Some json -> json
+           | None -> raise MalformedJSON
+         in let db = db_from_req req
+         in let tags = tag_list_from_json json
+         in let elems = Db.tag_search db (Db.list_of_elements db) tags
+         in `List (List.map
+                     (fun elem -> elem |> Db.entry_of_element |> Entry.to_json)
+                     elems)
+            |> Yojson.Basic.to_string
+            |> Yojson.Safe.from_string
+            |> Response.of_json
+            |> Lwt.return
+       end with
+       | e -> exn_response e
     )
 
 let add_handler req = 
-  let json_req = Request.to_json_exn req
-  in Lwt.bind
-    json_req
+  Lwt.bind
+    (Request.to_json req)
     (fun json -> 
-       (try begin
-          let ent = json
-                    |> Yojson.Safe.to_basic
-                    |> Entry.from_json
-          in let db = db_from_req req
-          in let elem = Db.create_element
-                 (Entry.id ent)
-                 (Entry.loc ent)
-                 (Entry.tags ent)
-                 (Entry.to_json ent)
-          in let () = Db.add db elem
-          in "Success"
-        end with
-        | Failure s -> s
-       )
-       |> Response.of_plain_text
-       |> Lwt.return
+       try begin
+         let json = match json with
+           | Some json -> json
+           | None -> raise MalformedJSON
+         in let ent = json
+                      |> Yojson.Safe.to_basic
+                      |> Entry.from_json
+         in let db = db_from_req req
+         in let elem = Db.create_element
+                (Entry.id ent)
+                (Entry.loc ent)
+                (Entry.tags ent)
+                (Entry.to_json ent)
+         in let () = Db.add db elem
+         in "Success"
+            |> Response.of_plain_text
+            |> Lwt.return
+       end with
+       | e -> exn_response e
     )
-(*let db = Registry.get reg (Router.param req "name")
-  in let entry = Request.to_json req *) (*Lwt.bind 
-                                               (Body.to_string req.body)
-                                               (fun ent -> Db.add db (Entry.from_json (Yojson.Basic.from_string ent)); Lwt.return_unit)*)
 
 let delete_handler req = 
-  let db = db_from_req req
-  in let id = get_req_param "id" req
-  in let ent = begin
-      try Db.find db id with
-      | Not_found -> failwith "entry not found"
-    end
-  in let () = Db.remove db ent
-  in "Success"
-     |> Response.of_plain_text
-     |> Lwt.return
+  try 
+    let db = db_from_req req
+    in let id = get_req_param "id" req
+    in let ent = begin
+        try Db.find db id with
+        | Not_found -> raise (EntryNotFound id)
+      end
+    in let () = Db.remove db ent
+    in "Success"
+       |> Response.of_plain_text
+       |> Lwt.return
+  with
+  | e -> exn_response e
 
 let write_handler req =
-  let db = db_from_req req
-  in let filename = (Db.name db) ^ ".db"
-  in let () = Db.to_list_json db filename
-  in "Success"
-     |> Response.of_plain_text
-     |> Lwt.return
-
-let print_param_handler req =
-  Printf.sprintf "Hello, %s\n" (Router.param req "name")
-  |> Response.of_plain_text
-  |> Lwt.return
+  try 
+    let db = db_from_req req
+    in let filename = (Db.name db) ^ ".db"
+    in let () = Db.to_list_json db filename
+    in "Success"
+       |> Response.of_plain_text
+       |> Lwt.return
+  with
+  | e -> exn_response e
 
 let start () =
   App.empty
-  |> App.get "/hello/:name" print_param_handler
   |> App.post "/initialize" initialize_handler
   |> App.post "/add" add_handler
   |> App.get "/tag-search" tag_search_handler
