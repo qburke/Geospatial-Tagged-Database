7a9905a4e158b407cec18ba67b562a0eafdb5d42
Quin Burke <qjb5@cornell.edu>
2020-12-17 00:57:42 -0500

    Add registry,ml files

---
+++ lib/registry.mli
@@ -0,0 +1,18 @@
+(** type t is the type of database registry *)
+type t
+
+(** [init ()] is a new empty database registry. *)
+val init : unit -> t
+
+(** [load_from_file r file name] loads the database from [file] into registry
+    [r] with name [name] and returns the key by which to fetch the newly loaded
+    database. If the database is already loaded in [r], it will be treated as a
+    cloned instance of the database. *)
+val load_from_file : t -> string -> string -> string
+
+(** [add_new r name] adds a new empty database to the registry [r] named [name]
+    and returns the key by which to fetch the newly loaded database. *)
+val add_new : t -> string -> string
+
+(** [get r k] is the database corresponding to key [k] in registry [r]. *)
+val get :  t -> string -> Db.database
