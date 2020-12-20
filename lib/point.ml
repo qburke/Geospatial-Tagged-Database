c0c81c1bf89e7f5a8d7051ba4a8ce66368295932
yueshen99 <vyt3@cornell.edu>
2020-12-19 20:50:04 -0500

    test

--- lib/point.ml
+++ lib/point.ml
@@ -6,10 +6,8 @@ let to_json (t : t) : Yojson.Basic.t =
   `Assoc [("x", `Float (fst t)); ("y", `Float (snd t))]
 
 let to_string (x, y) = 
-  "(" ^ string_of_float x ^ ", " 
-  ^ string_of_float y ^ ")"
+  "(" ^ string_of_float x ^ ", " ^ string_of_float y ^ ")"
 
-(** [distance p1 p2] is distance between point [p1] and point [p2]. *)
 let distance (x0,y0) (x1,y1) =
   Float.hypot (x0 -. x1) (y0 -. y1)
 
